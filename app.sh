ed190dc595501315dbf710b1d8b506c5fa1acb27
payalacharan <payalacharan@gmail.com>
2026-02-28 14:55:34 +0530

    Added Public address of Instance

--- app.sh
+++ app.sh
@@ -2,7 +2,8 @@
 echo "Starting Application Build"
 echo "Hello from GitHub Project"
 echo "CI Pipeline Initialisation"
-echo "Third Commit"
+iecho "Third Commit"
 echo "Hi chara it's successn"
 echo "Today is Saturday, Had Your Breakfast and Lunch?"
+echo "It's Working "
 date
